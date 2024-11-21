ab13fc304e4073d5c626c7ed8980d354040395b5
lltosso <luis.tosso@gmail.com>
2024-11-21 09:56:58 +0100

    java scanner

--- src/Bot.java
+++ src/Bot.java
@@ -1,18 +1,23 @@
+import java.util.Scanner;
+
 public class Bot {
     public static void main(String[] args) {
         //Crea el scanner
-
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hola, ¿Cómo te llamas?");
+        String nombre = scanner.nextLine();
+        scanner.close();
 
         //Recoge el dato
         //Añade una nueva línea antes de mostrar la siguiente pregunta.
 
-        System.out.println("Hola <name>! Soy un Bot. ¿De qué ciudad eres?");
-            hola
+        System.out.println("Hola"  + nombre + "Soy un Bot. ¿De qué ciudad eres?");
+
+
         //Recoge el dato
         //Añade una nueva línea antes de mostrar la siguiente pregunta.
 
-        System.out.println("He escuchado que <location> es un encanto. Yo nací en Oracle city.");
+        System.out.println("He escuchado que"  + location + "es un encanto. Yo nací en Oracle city.");
         System.out.println("\n¿Cuántos años tienes?");
 
         //Recoge el dato
